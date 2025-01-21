94f4b65cde902877527d9d8091b7b432463cdd18
Jonas Alberto Pereira <jopereir@c1r1p2.42sp.org.br>
2025-01-21 12:08:54 -0300

    update: binary tree(fork) - ok

--- Labs/Jonas/binary_tree.c
+++ Labs/Jonas/binary_tree.c
@@ -6,20 +6,27 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 10:40:06 by jopereir          #+#    #+#             */
-/*   Updated: 2025/01/21 10:54:37 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/01/21 12:07:31 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
 
 typedef struct s_tree
 {
-	int				value;
+	pid_t			pid;
 
 	struct s_tree	*left;
 	struct s_tree	*right;
 }	t_tree;
+
+void	hello(void);
+void	destroy_tree(t_tree *root);
 
 t_tree	*new_tree(int value)
 {
@@ -28,9 +35,14 @@ t_tree	*new_tree(int value)
 	new = malloc(sizeof(t_tree));
 	if (!new)
 		return (NULL);
-	new->value = value;
+	new->pid = value;
 	new->left = NULL;
 	new->right = NULL;
+	if (new->pid == -1)
+		exit(1);
+	if (new->pid == 0)
+		hello();
+	waitpid(new->pid, NULL, 0);
 	return (new);
 }
 
@@ -38,7 +50,7 @@ t_tree	*add_tree(t_tree *root, int value)
 {
 	if (!root)
 		return (new_tree(value));
-	if (value < root->value)
+	if (value < root->pid)
 		root->left = add_tree(root->left, value);
 	else
 		root->right = add_tree(root->right, value);
@@ -50,7 +62,7 @@ void	print_tree(t_tree *root)
 	if (!root)
 		return ;
 	print_tree(root->left);
-	printf("%d ", root->value);
+	printf("%d ", root->pid);
 	print_tree(root->right);
 }
 
@@ -63,18 +75,22 @@ void	destroy_tree(t_tree *root)
 	free(root);
 }
 
+void	hello(void)
+{
+	printf("Hello from %d\n", getpid());
+}
+
 int	main(void)
 {
 	t_tree	*node;
+	t_tree	*temp;
 
-	node = new_tree(15);
-	node = add_tree(node, 25);
-	node = add_tree(node, 4);
-	node = add_tree(node, 2);
-	node = add_tree(node, 10);
-	node = add_tree(node, 100);
-	node = add_tree(node, 99);
-	print_tree(node);
-	printf("\n");
-	destroy_tree(node);
+	node = new_tree(fork());
+	if (node->pid != 0)
+	{
+		node = add_tree(node, fork());
+		node = add_tree(node, fork());
+		node = add_tree(node, fork());
+		destroy_tree(node);
+	}
 }
