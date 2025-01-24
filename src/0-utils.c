64d3b74fa2acb92ae1a4665189850e17a4be166a
Jonas Alberto Pereira <jopereir@c1r1p2.42sp.org.br>
2025-01-24 13:48:00 -0300

    update: exec one single command

--- src/0-utils.c
+++ src/0-utils.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 13:15:20 by jopereir          #+#    #+#             */
-/*   Updated: 2025/01/24 13:17:05 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/01/24 13:47:17 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ void	*clear_split(char **str)
 {
 	int	i;
 
+	if (!str)
+		return (NULL);
 	i = 0;
 	while (str[i])
-		free(str[i]);
+		free(str[i++]);
 	free(str);
 	return (NULL);
 }
