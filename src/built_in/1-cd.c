4c3e5ed47fc5ad9ef12b776bf42e586f769b935b
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-01-28 20:02:58 -0300

    ok

--- src/built_in/1-cd.c
+++ src/built_in/1-cd.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:24:06 by jopereir          #+#    #+#             */
-/*   Updated: 2025/01/27 15:24:39 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/01/28 20:02:15 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 void	ft_cd(char *input)
 {
-	if (chdir(&input[3]) < 0)
-		printf("cd: arquivo ou diretório inexistente: %s\n", &input[3]);
+	char	*path;
+
+	path = NULL;
+	if (input[3] == '~')
+		path = getenv("HOME");
+	else if (input[3] == '-')
+		path = getenv("OLDPWD");
+	if (path == NULL)
+	{
+		if (chdir(input + 3) == -1)
+			ft_printf("No such file or directory %s\n", input + 3);
+	}
+	else
+		chdir(path);
 }
--- src/main/1-main.c
+++ src/main/1-main.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 13:09:08 by jopereir          #+#    #+#             */
-/*   Updated: 2025/01/28 11:38:14 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/01/28 19:46:32 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ static void	handle_signal(int signal)
 {
 	if (signal == SIGINT)
 		printf("\n\033[1;36m<<Master of universe(mini)>>$ \033[0m");
+	if (signal == SIGQUIT)
+		return ;
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -24,6 +26,7 @@ int	main(int argc, char **argv, char **envp)
 
 	(void)argv;
 	signal(SIGINT, handle_signal);
+	signal(SIGQUIT, handle_signal);
 	if (argc != 1)
 		return (1);
 	create(&data, envp);
