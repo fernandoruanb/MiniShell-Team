f18656a3fcc0fdb3274b1c2e81e3cc5caf0daf28
Jonas Alberto Pereira <jopereir@c1r1p2.42sp.org.br>
2025-01-30 11:24:33 -0300

    e

--- src/main/1-main.c
+++ src/main/1-main.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 13:09:08 by jopereir          #+#    #+#             */
-/*   Updated: 2025/01/30 11:13:24 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/01/30 11:23:54 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,14 @@ int	is_operator(char c)
 	return (c == '|' || c == '<' || c == '>' || c == '$');
 }
 
+char	**get_elements(char *str){
+	char	**argv;
+	int		i;
+
+		
+	return (argv);
+}
+
 t_token	*new_token(char *input)
 {
 	t_token	*new;
@@ -50,22 +58,31 @@ t_token	*new_token(char *input)
 	new = ft_calloc(1, sizeof(t_token));
 	if (!new)
 		return (NULL);
-	new->argv = get_elements(input, &new->argc);
+	new->argv = ft_split(input, ' ');
 	new->next = NULL;
 	return (new);
 }
 
 void	print_argv(char **str){
-	int	i;
+	int	i, j;
 
 	i = 0;
-	
+	while (str[i]){
+		j = 0;
+		while (str[i][j])
+			printf("%c", str[i][j++]);
+		printf("\n");
+		i++;
+	}
 }
 
 int	main(void){
 	char		*cmd;
 	t_token		*tokens;
 
+	signal(SIGINT, handle_signal);
 	cmd = ft_strdup("ls -l | wc -l");
+	tokens = new_token(cmd);
+	print_argv(tokens->argv);
 	return (0);
 }
