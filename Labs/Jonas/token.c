a72f7c3c21eaabd9970781ff8cbdbb2045b914cf
Jonas Alberto Pereira <jopereir@c1r1p2.42sp.org.br>
2025-01-30 10:54:37 -0300

    .

--- Labs/Jonas/token.c
+++ Labs/Jonas/token.c
@@ -6,7 +6,47 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 15:48:06 by jopereir          #+#    #+#             */
-/*   Updated: 2025/01/24 15:48:07 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/01/30 10:43:05 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+
+char	*ft_strdup(const char *s)
+{
+	char	*buffer;
+	int		length;
+	int		index;
+
+	index = 0;
+	length = strlen(s);
+	buffer = (char *)malloc(length + 1);
+	if (!buffer)
+		return (NULL);
+	while (s[index] != '\0')
+	{
+		buffer[index] = s[index];
+		index++;
+	}
+	buffer[index] = '\0';
+	return (buffer);
+}
+
+typedef struct s_token
+{
+	char	**argv;
+	int		argc;
+}	t_token;
+
+
+int	main(void){
+	char	*cmd;
+
+	cmd = ft_strdup("ls -l | wc -l");
+	return (0);
+}
--- src/main/1-main.c
+++ src/main/1-main.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 13:09:08 by jopereir          #+#    #+#             */
-/*   Updated: 2025/01/28 15:43:09 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/01/30 10:53:41 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,48 @@ static void	handle_signal(int signal)
 		printf("\n\033[1;36m<<Master of universe(mini)>>$ \033[0m");
 }
 
-int	main(int argc, char **argv, char **envp)
-{
-	t_data	data;
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	t_data	data;
 
-	(void)argv;
-	signal(SIGINT, handle_signal);
-	if (argc != 1)
-		return (1);
-	create(&data, envp);
+// 	(void)argv;
+// 	signal(SIGINT, handle_signal);
+// 	if (argc != 1)
+// 		return (1);
+// 	create(&data, envp);
+// 	return (0);
+// }
+
+typedef struct s_token
+{
+	char			**argv;
+	int				argc;
+
+	struct s_token	*next;	
+}	t_token;
+
+int	is_operator(char c)
+{
+	return (c == '|' || c == '<' || c == '>' || c == '$');
+}
+
+t_token	*new_token(char *input)
+{
+	t_token	*new;
+
+	new = ft_calloc(1, sizeof(t_token));
+	if (!new)
+		return (NULL);
+	new->argv = get_elements(input, &new->argc);
+	new->next = NULL;
+	return (new);
+}
+
+int	main(void){
+	char		*cmd;
+	t_token		*tokens;
+
+	cmd = ft_strdup("ls -l | wc -l");
+	
 	return (0);
 }
