a519a58076f246199271a55dcf4cee98814f9aed
Jonas <jonasper19@gmail.com>
2025-02-06 13:08:18 -0300

    cpy cpy

---
+++ Labs/Fernando/MINISHELL/srcs/syntax_check/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int	main (void)
+{
+	string	name;
+
+	cout << "Type your name: ";
+	cin >> name;
+	cout << "hello world" << endl;
+	return (0);
+}
