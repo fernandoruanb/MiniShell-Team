739453843fae8d5011ae4dc62510c28b25120a9f
Jonas Alberto Pereira <jopereir@c1r1p2.42sp.org.br>
2025-02-07 16:26:12 -0300

    exit

--- src/built_in/2-exit.c
+++ src/built_in/2-exit.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 11:47:20 by jopereir          #+#    #+#             */
-/*   Updated: 2025/02/07 11:32:05 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/02/07 16:25:56 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static int	is_all_num(char *str)
 {
 	int	i;
 
-	i = 0;
+	i = -1;
 	while (str[i])
 		if (!ft_isdigit(str[i]))
 			return (0);
@@ -26,10 +26,9 @@ static int	is_all_num(char *str)
 int	ft_exit(char *name, t_data *data, char *exit_status)
 {
 	rl_clear_history();
-	ft_double_free(name, NULL);
+	free(name);
 	data->prompt->exit_status = 0;
-	if (exit_status)
-		if (!is_all_num(exit_status))
-			data->prompt->exit_status = ft_atoi(exit_status);
+	if (exit_status && is_all_num(exit_status))
+		return (destroy(data, "Exit", ft_atoi(exit_status)));
 	return (destroy(data, "Exit", data->prompt->exit_status));
 }
