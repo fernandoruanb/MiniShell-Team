d4ee35f2a3596090be97d2dfe0322d536597f73b
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-02-10 14:19:47 -0300

    ok

--- src/syntax_check/final_check.c
+++ src/syntax_check/final_check.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 08:42:44 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/10 08:58:38 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/02/10 14:19:18 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int	final_check(t_utils *data)
 	if (is_absolute_path_quotes(data) || test_all_paths(data)
 		|| case_builtins_quotes(data))
 		return (1);
-	return (show_error_fd("Invalid thing", 0, data, 0));
+	return (0);
 }
