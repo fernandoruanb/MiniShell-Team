ca5d7ad297787c0517ff78000748e7d38370b1b0
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-02-10 13:14:38 -0300

    ok

--- src/syntax_check/check_quotes.c
+++ src/syntax_check/check_quotes.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 12:47:09 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/10 12:48:09 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/02/10 13:14:17 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 int	check_quotes(t_token *root)
 {
-	int	index;
+	int		index;
 	char	quote;
-	int	flag;
+	int		flag;
 
 	index = 0;
 	flag = 0;
 	while (root->str[index] != '\0')
 	{
-		if ((flag == 1) && (root->str[index] == '\'' || root->str[index] == '\"'))
+		if ((flag == 1) && (root->str[index] == '\''
+				|| root->str[index] == '\"'))
 		{
 			if (quote != root->str[index])
 				return (0);
