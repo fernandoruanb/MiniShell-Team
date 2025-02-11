7c654b412e884c20e6390423adc84569aeb60ada
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-02-11 16:05:18 -0300

    ok

--- src/syntax_check/case_command.c
+++ src/syntax_check/case_command.c
@@ -6,18 +6,27 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 12:21:51 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/11 13:43:12 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/02/11 16:02:57 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+static int	extra_cases_commands(t_token *root, t_utils *data)
+{
+	if ((root->id == CMD) && (exist_command(root,data)
+			|| check_absolute_path(root, data)
+			|| is_insider_quotes(root, data) || special(root, data)))
+	{
+		data->status = 1;
+		return (1);
+	}
+	return (0);
+}
 int	case_command(t_token *root, t_utils *data)
 {
 	if ((root->id == CMD && data->status > 1) && (exist_command(root, data)
-			|| check_absolute_path(root, data)
-			|| is_insider_quotes(root, data)
-			|| special(root, data)))
+			|| check_absolute_path(root, data)))
 		return (decrement_status(data));
 	else if (case_builtins(root) || is_environment(root)
 		|| is_insider_quotes(root, data) || special(root, data))
@@ -36,5 +45,7 @@ int	case_command(t_token *root, t_utils *data)
 		return (1);
 	else if (root->id == CMD && data->status == 1)
 		return (show_error_fd("CMD received in ARG mode", 0, data, 0));
+	else if (extra_cases_commands(root, data))
+		return (1);
 	return (show_error_fd("Unknown CMD syntax", 0, data, 0));
 }
--- src/syntax_check/final_check.c
+++ src/syntax_check/final_check.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 08:42:44 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/11 13:35:37 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/02/11 15:43:52 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	final_check(t_utils *data)
 {
-	if (test_all_paths(data))
+	if (case_builtins_quotes(data) || test_all_paths(data)
+		|| is_absolute_path_quotes(data))
 		return (1);
 	return (0);
 }
