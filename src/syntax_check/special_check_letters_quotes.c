d55c60d9f2884e2f5241c596089aebb0283f84fa
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-02-13 10:50:05 -0300

    ok

--- src/syntax_check/special_check_letters_quotes.c
+++ src/syntax_check/special_check_letters_quotes.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 12:48:46 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/11 17:50:50 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/02/13 10:48:00 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,12 @@ int	special_check_quotes(t_token *root, t_utils *data)
 	{
 		if ((root->str[index] == '\'' || root->str[index] == '\"'))
 		{
-			if (root->str[index] == '\'' && root->str[index - 1] != '\\')
+			if (root->str[0] == '\\')
+				data->simple_quotes++;
+			else if (root->str[0] == '\"')
+				data->double_quotes++;
+			else if (root->str[index] == '\''
+				&& root->str[index - 1] != '\\')
 				data->simple_quotes++;
 			else if (root->str[index - 1] != '\\')
 				data->double_quotes++;
