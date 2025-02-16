a3f033b34cf54c683b3d36ef0daf5901c52ccf85
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-02-16 20:03:09 -0300

    ok

--- src/syntax_check/how_many_quotes.c
+++ src/syntax_check/how_many_quotes.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 12:50:50 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/16 18:01:31 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/02/16 19:56:07 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,20 @@ int	check_final_quotes(t_token *root)
 
 	index = 0;
 	if (root->str[0] == '\\')
-		index = 2;
+		index += 2;
 	while (root->str[index] != '\0')
 	{
-		if (root->str[index - 1] != '\\' && (root->str[index] == '\''
+		if ((root->str[index - 1] != '\\') && (root->str[index] == '\''
 				|| root->str[index] == '\"'))
 		{
 			quote = root->str[index];
 			index++;
 			while (root->str[index] != '\0' && root->str[index] != quote)
+			{
+				if (root->str[index] == '\\')
+					index++;
 				index++;
+			}
 			if (root->str[index] == '\0')
 				return (0);
 		}
