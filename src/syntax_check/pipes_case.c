233e612eef7e5282e6f2c4bba8cf8abd1797e8ad
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-02-20 09:11:32 -0300

    ok

--- src/syntax_check/pipes_case.c
+++ src/syntax_check/pipes_case.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 13:41:58 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/19 12:04:39 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/02/20 09:09:23 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,13 @@ int	pipes_case(t_token *root, t_utils *data)
 	last = root;
 	while (last->previous)
 	{
-		if (last->id == PIPE)
+		if (last->id == PIPE || last->id == OPERATOR_AND
+			|| last->id == OPERATOR_OR)
 		{
 			flag = 2;
 			break ;
 		}
 		last = last->previous;
 	}
-	/*last = root;
-	while (last->next)
-	{
-		if (last->id == PIPE)
-		{
-			flag++;
-			break ;
-		}
-		last = last->next;
-	}*/
 	return (godness_decision(flag, data));
 }
--- src/syntax_check/start_case.c
+++ src/syntax_check/start_case.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 17:24:39 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/16 17:59:41 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/02/20 09:09:49 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ int	start_case(t_token *root, t_utils *data)
 	last = root;
 	while (last->next)
 	{
-		if (last->id == PIPE)
+		if (last->id == PIPE || last->id == OPERATOR_AND
+			|| last->id == OPERATOR_OR)
 		{
 			data->status = 1;
 			return (1);
