69a15a6f42bf9bcc35be93b2cd0d802fb9fa4961
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-02-24 14:21:45 -0300

    ok

--- src/handle_operators/handle_pipe_op.c
+++ src/handle_operators/handle_pipe_op.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 15:45:01 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/24 12:40:17 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/02/24 14:21:32 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	read_mode(char **cmd, int *pipefd, t_utils *data)
 	if (pid == 0)
 	{
 		if (dup2(data->fd_backup, STDIN_FILENO) == -1)
-			return (0);
+			exit(EXIT_FAILURE);
 		close_descriptors(pipefd, 1, data);
 		if (execve(cmd[0], cmd, data->envp) == -1)
 		{
@@ -46,7 +46,7 @@ int	write_mode(char **cmd, int *pipefd, t_utils *data)
 	{
 		data->fd_backup = dup(pipefd[0]);
 		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
-			exit(1);
+			exit(EXIT_FAILURE);
 		close_descriptors(pipefd, 0, data);
 		if (execve(cmd[0], cmd, data->envp) == -1)
 		{
@@ -69,9 +69,9 @@ int	write_read_mode(char **cmd, int *pipefd, t_utils *data)
 	if (pid == 0)
 	{
 		if (dup2(data->fd_backup, STDIN_FILENO) == -1)
-			return (0);
+			exit(EXIT_FAILURE);
 		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
-			return (0);
+			exit(EXIT_FAILURE);
 		fulfil_data_fd(pipefd, data);
 		close_descriptors(pipefd, 1, data);
 		if (execve(cmd[0], cmd, data->envp) == -1)
@@ -89,8 +89,6 @@ int	handle_pipe_op(char *cmd, int flag, t_utils *data)
 {
 	int		pipefd[2];
 	char	**split1;
-	int	result;
-
 	if (pipe(pipefd) == -1)
 		return (0);
 	split1 = ft_split(cmd, ' ');
@@ -99,28 +97,27 @@ int	handle_pipe_op(char *cmd, int flag, t_utils *data)
 		close_descriptors(pipefd, 0, data);
 		return (0);
 	}
-	result = 0;
 	if (flag == 1)
-		result = write_mode(split1, pipefd, data);
+		write_mode(split1, pipefd, data);
 	else if (flag == 2)
-		result = read_mode(split1, pipefd, data);
+		read_mode(split1, pipefd, data);
 	else if (flag == 3)
-		result = write_read_mode(split1, pipefd, data);
+		write_read_mode(split1, pipefd, data);
 	if (flag == 3 || flag == 1)
 		close_descriptors(pipefd, 0, data);
 	if (flag == 2)
 		close_descriptors(pipefd, 1, data);
-	return (result);
+	return (data->exec_status);
 }
 
-int	main(int argc, char **argv, char **envp)
+/*int	main(int argc, char **argv, char **envp)
 {
 	t_data	data;
 
 	if (argc < 3)
 		return (1);
 	data.utils.envp = envp;
-	if (handle_pipe_op(argv[1], 1, &data.utils) != 0)
+	if (handle_pipe_op(argv[1], 1, &data.utils))
 		handle_pipe_op(argv[2], 2, &data.utils);
 	return (0);
-}
+}*/
