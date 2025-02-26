7506a1bf6954fcab420f562dc0ee30197d976c85
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-02-26 13:45:22 -0300

    oK

--- src/handle_operators/ft_read_mode.c
+++ src/handle_operators/ft_read_mode.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 10:05:17 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/26 10:52:36 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/02/26 13:34:36 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	ft_read_mode(char **cmd, t_utils *data)
 			exit(127);
 		else if (errno == EACCES)
 			exit(126);
-		exit(errno);
+		else
+			exit(errno);
 	}
 }
