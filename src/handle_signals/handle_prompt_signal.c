08a61d52172b5cdda3c7108a439a76f3d64c3172
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-03-05 10:57:28 -0300

    ok

--- src/handle_signals/handle_prompt_signal.c
+++ src/handle_signals/handle_prompt_signal.c
@@ -6,22 +6,20 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 17:07:46 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/26 17:24:43 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/03/04 18:13:04 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static void	handle_prompt_signal(int signal)
+static void	handle_prompt(int signal)
 {
 	if (signal == SIGINT)
-		ft_printf(BLUE"Master of universe(mini)$ "RESET);
-	if (signal == SIGQUIT)
-		return ;
+		ft_printf(BLUE"\nMaster of universe(mini)$ "RESET);
 }
 
-void	handle_prompt_signal(int signal)
+void	handle_prompt_signal(void)
 {
-	signal(SIGINT, handle_prompt_sig);
-	signal(SIGQUIT, handle_prompt_sig);
+	signal(SIGINT, handle_prompt);
+	signal(SIGQUIT, SIG_IGN);
 }
