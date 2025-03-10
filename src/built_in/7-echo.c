4fa28cf28d46ab0b6755f049607b27f31166b657
Jonas Alberto Pereira <jopereir@c1r1p2.42sp.org.br>
2025-03-10 11:15:34 -0300

    update: refatoração echo

--- src/built_in/7-echo.c
+++ src/built_in/7-echo.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 10:43:23 by jopereir          #+#    #+#             */
-/*   Updated: 2025/03/10 11:00:48 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/03/10 11:15:07 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 int	ft_echo(char **str)
 {
 	int	i;
+	int	nl;
 
-	printf("echo built in \n");
 	if (!str)
 		return (!printf("\n"));
-	i = 0;
+	nl = ft_strcmp(str[0], "-n") != 0;
+	i = !nl;
 	while (str[i])
-		printf("%s", str[i++]);
-	printf("\n");
+		printf("%s ", str[i++]);
+	if (nl)
+		printf("\n");
 	return (0);
 }
