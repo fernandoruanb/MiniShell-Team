56d98bc88c1558fd7a31660df4b81ed2399f4240
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-03-13 14:03:18 -0300

    exit code 130 solved

--- src/handle_operators/translate.c
+++ src/handle_operators/translate.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 10:50:15 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/03/07 17:47:48 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/03/13 14:01:49 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	translate(t_data *data)
 {
 	if (data->utils.exec_status == 32512)
 		data->utils.exec_status = 127;
+	else if (data->utils.exec_status == 2)
+		data->utils.exec_status = 130;
 	else if (data->utils.exec_status == 32256
 		|| data->utils.exec_status == 3328)
 		data->utils.exec_status = 126;
