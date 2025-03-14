6fd9d4539de448f288a62f2c770741b7d641b5aa
Jonas <jonasper19@gmail.com>
2025-03-14 18:37:58 -0300

    ;

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 18:26:15 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/03/07 12:49:28 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/03/14 17:45:04 by jonas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	length;
 
-	if (!s)
+	if (!s || s[0] == '\0')
 		return (0);
 	length = 0;
 	while (s[length] != '\0')
--- src/parsing/3-domain_expansion.c
+++ src/parsing/3-domain_expansion.c
@@ -6,7 +6,7 @@
 /*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 13:24:54 by jonas             #+#    #+#             */
-/*   Updated: 2025/03/14 16:15:59 by jonas            ###   ########.fr       */
+/*   Updated: 2025/03/14 18:31:24 by jonas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,13 @@
 // 	return (str);
 // }
 
-static char	*is_return(t_data *data, char *find)
+static char	*is_return(t_data *data, int *i)
 {
-	free(find);
 	if (data->prompt->exit_status > 255)
 		data->prompt->exit_status = WEXITSTATUS(data->prompt->exit_status);
+	printf("i antes: %d\n", *i);
+	*i += 2;
+	printf("i dps: %d\n", *i);
 	return (ft_itoa(data->prompt->exit_status));
 }
 
@@ -61,15 +63,29 @@ static char	*get_find(char *str, int n, int *i)
 	char	*new;
 	char	*temp;
 
+	(void)i;
 	new = ft_strndup(str, n);
 	if (find_quote(new))
 	{
-		(*i)--;
+		//(*i)--;
 		temp = ft_strndup(new, int_ft_strlen(new) - 1);
 		free(new);
 		return (temp);
 	}
 	return (new);
+}
+
+int	get_j(char *str, int i)
+{
+	int	j;
+
+	j = 0;
+	while (str[i] && str[i] != ' ' && str[i] != '\"')
+	{
+		i++;
+		j += str[i] != '\"';
+	}
+	return (j);
 }
 
 static char	*search_content(char *str, t_data *data, int *i)
@@ -79,18 +95,16 @@ static char	*search_content(char *str, t_data *data, int *i)
 	t_export	*temp;
 	t_localvar	*temp2;
 
-	j = 0;
-	while (str[*i] && str[*i] != ' ' && str[*i] != '\"' && str[*i] != '\\')
-	{
-		(*i)++;
-		j++;
-	}
-	find = get_find(&str[*i - j + 1], j - 1, i);
-	if (ft_strnstr(str, "$?", ft_strlen(str)))
-		return (is_return(data, find));
+	if (!ft_strncmp(str, "$?", 2))
+		return (is_return(data, i));
+	j = get_j(str, *i);
+	printf("j: %d i: %d\n", j, *i);
+	find = get_find(&str[++(*i)], j, i);
+	printf("find: %s\n", find);
 	temp = search_var(&data->export_vars, find);
 	if (temp)
 	{
+		printf("achei\n");
 		free(find);
 		return (ft_strdup(temp->value));
 	}
@@ -106,6 +120,7 @@ static char	*complete_str(char *str, char *expand, char *prev, int *i)
 	char	*tmp;
 	char	*temp;
 
+	tmp = NULL;
 	if (!expand)
 	{
 		tmp = ft_strjoin(prev, &str[*i]);
@@ -115,6 +130,11 @@ static char	*complete_str(char *str, char *expand, char *prev, int *i)
 	}
 	tmp = ft_strjoin(prev, expand);
 	ft_double_free(prev, expand);
+	if (*i >= int_ft_strlen(str))
+	{
+		free(str);
+		return (tmp);
+	}
 	temp = ft_strjoin(tmp, &str[*i]);
 	ft_double_free(tmp, str);
 	*i = 0;
@@ -127,18 +147,24 @@ char	*domain_expansion(char *str, t_data *data)
 	char	*expand;
 	char	*temp;
 
+	printf("string: %s\n", str);
 	if ((str[0] == '\'' && str[int_ft_strlen(str) - 1] == '\'')
 		|| (str[1] == ' ' || str[1] == '\0'))
 		return (str);
-	i = -1;
-	while (++i < int_ft_strlen(str))
+	i = 0;
+	while (i < int_ft_strlen(str))
 	{
 		if (str[i] == '$')
 		{
+			printf("i no loop: %d\n", i);
 			temp = ft_strndup(str, i);
+			printf("prev %s\n", temp);
 			expand = search_content(str, data, &i);
+			printf("expand: %s\n", expand);
 			str = complete_str(str, expand, temp, &i);
+			printf("str: %s\n", str);
 		}
+		i++;
 	}
 	return (str);
 }
