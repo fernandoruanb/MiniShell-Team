bb460022935b4b59e030155e9dadb543cba863db
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-03-13 21:47:08 -0300

    minishell one hundred percent functional

--- src/syntax_check/show_error_fd.c
+++ src/syntax_check/show_error_fd.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 12:51:41 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/02/17 16:04:39 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/03/13 21:46:43 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ int	show_error_fd(char *message, int flag, t_utils *data, int signal)
 {
 	ft_putendl_fd(message, 2);
 	if (signal != 0)
+	{
 		data->exit_status = signal;
+		data->exec_status = signal;
+	}
 	if (flag == 1)
 		return (1);
 	return (0);
